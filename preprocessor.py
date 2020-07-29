0a5a26fedd310dcf52ca1f6c27148e9c96b3d004
hsh2438 <hsh2438@naver.com>
2020-07-29 16:28:31 +0000

    update sources

---
+++ preprocessor.py
@@ -0,0 +1,101 @@
+import os
+
+class Preprocessor:
+
+    def __init__(self, data_dir, max_len):
+        self.data_dir = data_dir
+        self.max_len = max_len
+
+        self.PAD = '<PAD>'
+        self.UNK = '<UNK>'
+        self.vocab = [self.PAD, self.UNK]
+        self.labels = []
+
+    def __load_file__(self, data_file, splited=True):
+        data = []
+        with open(os.path.join(self.data_dir, data_file), 'r', encoding='utf-8') as fr:
+            for line in fr.readlines():
+                if splited:
+                    data.append(line.strip().split('\t'))
+                else:
+                    data.append(line.strip())
+        return data
+
+    # load train and test data
+    def load_data(self):
+        self.train_data = self.__load_file__('train.tsv')
+        self.test_data = self.__load_file__('test.tsv')
+
+    # load vocab from vocab.txt
+    def load_vocab(self):
+        self.vocab = self.__load_file__('vocab.txt', splited=False)
+    
+    # load labels from labels.txt
+    def load_labels(self):
+        self.labels = self.__load_file__('labels.txt', splited=False)
+
+    # make vocab and labels from train data
+    def make_vocab(self):
+        for line in self.train_data:
+            text, label = line[0], line[1]
+            for token in self.tokenize(text):
+                if not token in self.vocab:
+                    self.vocab.append(token)
+            if not label in self.labels:
+                self.labels.append(label)
+
+    # tokenize
+    def tokenize(self, text):
+        return text.split()
+    
+    # vectorize text
+    def make_feature(self, text):
+        feature = []
+        for token in self.tokenize(text):
+            if not token in self.vocab:
+                feature.append(self.vocab.index(self.UNK))
+            else:
+                feature.append(self.vocab.index(token))
+        while len(feature) < self.max_len:
+            feature.append(self.vocab.index(self.PAD))
+        return feature
+
+    def __make_features__(self, data):
+        features = []
+        labels = []
+
+        for line in self.train_data:
+            text, label = line[0], line[1]
+            feature = self.make_feature(text)
+            features.append(feature)
+            assert label in self.labels
+            labels.append(self.labels.index(label))
+
+        return features, labels
+
+    # vectorize train set
+    def make_train_features(self):
+        self.__train_features, self.__train_labels = self.__make_features__(self.train_data)
+
+    # vectorize test set
+    def make_test_features(self):
+        self.__test_features, self.__test_labels = self.__make_features__(self.test_data)
+    
+    # return vocab size
+    def get_vocab_size(self):
+        return len(self.vocab)
+    
+    # return number of labels
+    def get_num_labels(self):
+        return len(self.labels)
+    
+    def get_labels(self):
+        return self.labels
+
+    # return train features
+    def get_train_features(self):
+        return self.__train_features, self.__train_labels
+
+    # return test features
+    def get_test_features(self):
+        return self.__test_features, self.__test_labels
---
+++ service.py
@@ -0,0 +1,51 @@
+import os
+import tensorflow as tf
+
+from flask import Flask, request, jsonify
+from flask_restplus import Api, Resource, fields
+
+from preprocessor import Preprocessor
+from model import lstm_classifier_model
+
+
+# parameter
+data_dir = 'data'
+max_len = 64
+hidden_size = 64
+
+# create preprocessor
+preprocessor = Preprocessor(data_dir, max_len)
+preprocessor.load_vocab()
+preprocessor.load_labels()
+
+vocab_size = preprocessor.get_vocab_size()
+num_labels = preprocessor.get_num_labels()
+labels = preprocessor.get_labels()
+
+# create model
+model = lstm_classifier_model(vocab_size, max_len, hidden_size, num_labels)
+
+# load checkpoint
+model.load_weights(os.path.join(data_dir, 'checkpoint'))
+
+# flask setting
+app = Flask(__name__)
+api = Api(app, version='1.0', title='simple lstm classification')
+ns  = api.namespace('predict')
+
+text_api_model = api.model(name='text', model={
+    'text': fields.String(required=True, description='text', example='hello')
+})
+
+@ns.route('/')
+class Predict(Resource):
+    @api.expect(text_api_model)
+    def post(self):
+        text = request.json['text']
+        feature = preprocessor.make_feature(text)
+        prediction = model.predict([feature])
+        result = labels[tf.math.argmax(prediction[0])]
+        return jsonify(result)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=12341)
